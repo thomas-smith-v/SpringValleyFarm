4a5870bd552afd3f7caf48d44938fe9661bb1d23
thomas-smith-v <58493201+thomas-smith-v@users.noreply.github.com>
2020-04-22 19:56:47 -0400

    Created main loop

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
 import schedule
 import time
 import tests.system_test_031220 as t
+from src import weather_info, system_info, metrics
 
-schedule.every(5).minutes.do(t.test)
+def run_system_metrics():
+    cpu_temperature = system_info.get_cpu_temperature()
+    core_voltage = system_info.get_core_voltage()
+    cpu_percent = system_info.get_cpu_percent()
 
-t.test()
+    metrics.post_system_metric(name="cpu_temperature", value=cpu_temperature)
+    metrics.post_system_metric(name="core_voltage", value=core_voltage)
+    metrics.post_system_metric(name="cpu_percent", value=cpu_percent)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+def run_weather_metrics():
+    weather = weather_info.get_weather()
+    
+    temperature_forecast = weather_info.get_temperature_forecast(weather)
+    sky_cover_forecast = weather_info.get_sky_cover_forecast(weather)
 
+    current_temperature = weather_info.get_current(temperature_forecast)
+    current_sky_cover = weather_info.get_current(sky_cover_forecast)
+
+    metrics.post_weather_metric(name="temperature_forecast", value=temperature_forecast)
+    metrics.post_weather_metric(name="current_temperature", value=current_temperature)
+    metrics.post_weather_metric(name="sky_cover_forecast", value=sky_cover_forecast)
+    metrics.post_weather_metric(name="current_sky_cover", value=current_sky_cover)
+    
+
+
+def main():
+    # Initial upload
+    run_system_metrics()
+    run_weather_metrics()
+
+    # Schedule future uploads
+    schedule.every(5).minutes.do(run_system_metrics)
+    schedule.every(1).hour.do(run_weather_metrics)
+    
+    # Main loop
+    while True:
+        schedule.run_pending()
+        time.sleep(0.2)
+
+if __name__ == "__main__":
+    main()
